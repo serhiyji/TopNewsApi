2c1a5b60dc63e7d3dacfed3c7043c524373eefc9
serhiyji <serhiy22823@gmail.com>
2023-09-11 20:45:03 +0300

    Add project files.

---
+++ TopNewsApi.Api/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TopNewsApi.Core.Services;
+
+namespace TopNewsApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok((await _userService.GetAllAsync()).Payload);
+        }
+    }
+}
